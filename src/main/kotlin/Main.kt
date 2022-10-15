00e4524c784fcdf1675cae09377dc6071e2ec33a
Rico Georgi <rico-georgi@web.de>
2022-10-15 22:46:18 +0200

    git remote add origin https://github.com/RicoGeorgi/SimpleChattyBot.git git branch -M main git push -u origin main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,20 @@
 package bot
 
+import java.util.Scanner
+
 const val NAME: String = "Chatty"
 const val CREATION: String = "2022"
 
 fun main() {
 
+    val scanner = Scanner(System.`in`)
+
     println("Hello! My name is $NAME.")
     println("I was created in $CREATION.")
+    println("Please, remind me your name.")
+
+    val userName: String = scanner.next()
+
+    println("What a great name you have, $userName!")
 
 }
