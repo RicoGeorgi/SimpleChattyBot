c08a5b213d40152def36b7a67b42e331f94fda0a
Rico Georgi <rico-georgi@web.de>
2022-10-15 21:55:48 +0200

    git remote add origin https://github.com/RicoGeorgi/SimpleChattyBot.git git branch -M main git push -u origin main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package bot
+
+const val NAME: String = "Chatty"
+const val CREATION: String = "2022"
+
+fun main() {
+
+    println("Hello! My name is $NAME.")
+    println("I was created in $CREATION.")
+
+}
